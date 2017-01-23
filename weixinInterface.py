e2499b96b0a7bebaa78c353970b76e43d1ca96dd
MaxwellSnake <dont_bug_me@163.com>
2017-01-23 17:15:12 +0800

    测试封装

--- weixinInterface.py
+++ weixinInterface.py
@@ -110,7 +110,7 @@ class WeixinInterface:
                     return self.render.reply_text(fromUser,toUser,int(time.time()),u'叼毛，你手残输错啦，再来一次吧')
 
 
-    def handle_register(content):
+    def handle_register(self,content):
         if content == u'测试封装':
             # mc.set(fromUser+'_test','start_test') # 注册入口
             return self.render.reply_text(fromUser,toUser,int(time.time()),u'测试封装')
