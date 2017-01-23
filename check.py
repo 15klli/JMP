28aa12a14dbae1bfdad068e8e0d57b6ba6917ace
MaxwellSnake <dont_bug_me@163.com>
2017-01-24 01:40:49 +0800

    modify check.py

--- check.py
+++ check.py
@@ -1,5 +1,20 @@
 # _*_ coding:utf-8 _*_
+import web
+import web.db
+import sae.const
 
-def is_rollnum(num):
-	print('test success')
-   # return (num.startswith('20') and (len(num) == 10)
+
+db = web.database(
+    dbn='mysql',
+    host=sae.const.MYSQL_HOST,
+    port=int(sae.const.MYSQL_PORT),
+    user=sae.const.MYSQL_USER,
+    passwd=sae.const.MYSQL_PASS,
+    db=sae.const.MYSQL_DB
+)
+ 
+def addfk(username, fktime, fkcontent):
+    return db.insert('fk', user=username, time=fktime, fk_content=fkcontent)
+ 
+def get_fkcontent():
+    return db.select('fk', order='id')
