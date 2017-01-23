c7c31396f402ea87c78a345b9678011d7c02ff57
MaxwellSnake <dont_bug_me@163.com>
2017-01-24 02:37:08 +0800

    删除无用东西

--- check.py
+++ check.py
@@ -1,30 +1,4 @@
 # _*_ coding:utf-8 _*_
-import web
-import web.db
-import sae.const
-
-db = web.database(
-    dbn='mysql',
-    host=sae.const.MYSQL_HOST,
-    port=int(sae.const.MYSQL_PORT),
-    user=sae.const.MYSQL_USER,
-    passwd=sae.const.MYSQL_PASS,
-    db=sae.const.MYSQL_DB
-)
-
-
-def addfk(username, fktime, fkcontent):
-    return db.insert('fk', user=username, time=fktime, fk_content=fkcontent)
-
-
-def get_fkcontent():
-    return db.select('fk', order='id')
-
-
-def get_teststr():
-    return 'test_success'
-
-
 def is_rollnum(num):
     return (num.startswith('20')) and (len(num) == 10)
 
