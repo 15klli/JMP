acf6cc63a13a2f849748a813e1f6efd12b80a8bb
MaxwellSnake <dont_bug_me@163.com>
2017-01-24 02:35:53 +0800

    modify is_mail

--- check.py
+++ check.py
@@ -29,4 +29,4 @@ def is_rollnum(num):
     return (num.startswith('20')) and (len(num) == 10)
 
 def is_mail(content):
-    return content.find('@')    # todo：要用正则式检查的，这里先这样
+    return content.find('@') > 0   # todo：要用正则式检查的，这里先这样
